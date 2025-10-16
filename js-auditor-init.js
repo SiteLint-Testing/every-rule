79381da28f231e9c508a0919fbecaff6796ae09a
Jeff Wiesner <jwiesner@sitelint.com>
2025-10-16 09:43:39 -0400

    latest changes

--- js-auditor-init.js
+++ js-auditor-init.js
@@ -5,6 +5,6 @@
   a.id = 'auditor_app';
   a.dataset.autorun = '';
   a.dataset.cfasync = 'false';
-  a.src = 'https://stage-auditor.sitelint.com/auditor.bundle.js?tokenId=05f3493b1b369e6ff565cc61eb7154966c723f566e16f73fadac9d84c67aaf4c';
+  a.src = 'https://stage-auditor.sitelint.com/auditor.bundle.js?32c87bf7dee2424c7e0eb3cd04e5319b54d9a45cb7a8cc1919e414b4682d1bf1';
   m.parentNode.insertBefore(a, m);
 })(window, document, 'script');
